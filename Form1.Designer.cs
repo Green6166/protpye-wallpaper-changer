a05ecbdb2a3dcd7184cb65efa36a0688ad469f48
Green6166 <53530313+Green6166@users.noreply.github.com>
2022-01-14 13:57:00 +0000

    REWROTE IN C#
    REWROTE IN C# TO UPGRADE MAINTAINABILITY REDUCE SLUGGISHNESS AND TO BETTER INTEGRATE WITH A GUARANTEED METHOD THAT RIVALS THAT OF BIONIX WALLPAPER CHANGER

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace protpyesharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "2"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "\\\\utcn-dc-fs1\\student-home$\\2018\\U18J2GREEN\\My Pictures\\giphy.gif"});
+            this.listBox2.Location = new System.Drawing.Point(191, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(369, 186);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 101);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "add photo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 101);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DELETE ENTRY";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 101);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "EXECUTE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(120, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 101);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "PAUSE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(566, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 186);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(248, 204);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(143, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace protpyesharp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            button2.Enabled = false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             openFileDialog1.ShowDialog();
+           var  filenam = openFileDialog1.FileName;
+             listBox2.Items.Add(openFileDialog1.FileName);
+            listBox1.Items.Add(numericUpDown1.Value);
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+
+            if (filenam is null) 
+            {
+                listBox2.Items.RemoveAt(-1);
+                listBox1.Items.RemoveAt(-1);
+                  }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.RemoveAt(-1);
+            listBox1.Items.RemoveAt(-1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { button4.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            //setwallpaper();
+        }
+
+        private void setwallpaper()
+        { int pointer = 0;
+            while (button4.Enabled is true)
+            {
+                foreach (object item in listBox2.Items)
+                {
+                    pictureBox1.ImageLocation = item.ToString();
+                    timer1.Start();
+                    while (timer1.ToString() != listBox1.Items[pointer])
+                    {
+
+                    }
+                    pointer = pointer + 1;
+                    if (pointer>listBox1.Items.Count) {  button4.Enabled = false;  button1.Enabled = true;  button2.Enabled = true;  button3.Enabled = true;  }
+
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
